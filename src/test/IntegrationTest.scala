80624d72a7a30c064ef61a6b9fa47c7b0b1ad798
Robert Grider <robert.grider@northwestern.edu>
2015-08-14 13:42:15 -0500

    Handle a few simple error cases

--- src/test/IntegrationTest.scala
+++ src/test/IntegrationTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 import org.scalatest.concurrent.{ Conductors, Timeouts }
 import org.scalatest.time.SpanSugar._
 
-import org.zeromq.ZMQ, ZMQ.Context
+import org.zeromq.{ ZMQ, ZMQException }, ZMQ.Context
 
 class IntegrationTest extends FunSuite with Conductors with Timeouts {
   val context = ZMQ.context(1)
@@ -24,27 +24,47 @@ class IntegrationTest extends FunSuite with Conductors with Timeouts {
     def fetchResponse(address: String, reporter: String): String = {
       val reqSocket = context.socket(ZMQ.REQ)
       reqSocket.connect(address)
-      reqSocket.send(toZMQBytes(reporter))
-      reqSocket.setSocketOpt(ZMQ.ZMQ_RCVTIMEO, 3)
-      val reply = reqSocket.recv(0)
-      fromZMQBytes(reply)
+      reqSocket.setSendTimeOut(500)
+      val sent = reqSocket.send(toZMQBytes(reporter))
+      if (sent) {
+        reqSocket.setReceiveTimeOut(500)
+        Option(reqSocket.recv()).map(fromZMQBytes)
+          .getOrElse(throw new TimeoutException("no response received in time"))
+      }
+      else {
+        throw new TimeoutException("unable to establish connection")
+      }
     }
   }
 
   class Server(context: Context, address: String) extends Helpers {
+    import Server._
+
     val socket = context.socket(ZMQ.REP)
-    socket.bind(address)
+
+    try {
+      socket.bind(address)
+    } catch {
+      case z: ZMQException =>
+        throw new BindException("Unable to bind to " + address)
+    }
 
     def serve(f: String => String) = {
       val req = socket.recv(0)
       val reqString = fromZMQBytes(req)
       socket.send(toZMQBytes(f(reqString)), 0)
-      socket.close()
     }
 
-    def close(): Unit =
+    def close(): Unit = {
+      socket.unbind(address)
       socket.close()
+    }
   }
+
+  object Server {
+    class BindException(message: String) extends RuntimeException(message)
+  }
+
 
   test("simple client server") {
     val conductor = new Conductor
@@ -66,4 +86,25 @@ class IntegrationTest extends FunSuite with Conductors with Timeouts {
     }
   }
 
+  test("raises timeout exception when client cannot connect to server") {
+    intercept[TimeoutException] {
+      val client = new Client(context)
+      client.fetchResponse("inproc://nothere", "timeoutreq")
+    }
+  }
+
+  test("raises timeout exception when client cannot connect to tcp server") {
+    intercept[TimeoutException] {
+      val client = new Client(context)
+      client.fetchResponse("tcp://0.0.0.0:10101", "timeouttcpreq")
+    }
+  }
+
+  test("raises a BindException when the port cannot be bound") {
+    val server = new Server(context, "inproc://a")
+    intercept[Server.BindException] {
+      val server2 = new Server(context, "inproc://a")
+    }
+    server.close()
+  }
 }
